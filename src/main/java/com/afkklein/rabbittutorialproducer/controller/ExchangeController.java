92cd40a5aba96f153e8df04ca863b215e517a04b
Augusto Fabio Klein <augusto-klein@augusto.klein>
2020-08-13 17:38:15 -0300

    serializando manualmente o objeto

--- src/main/java/com/afkklein/rabbittutorialproducer/controller/ExchangeController.java
+++ src/main/java/com/afkklein/rabbittutorialproducer/controller/ExchangeController.java
@@ -1,13 +1,21 @@
 package com.afkklein.rabbittutorialproducer.controller;
 
 import com.afkklein.rabbittutorialproducer.domain.Person;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.UnsupportedEncodingException;
 
 @RestController
 @RequestMapping("exchanges")
@@ -26,9 +34,20 @@ public class ExchangeController {
     }
 
     @PostMapping("json/{exchange}/{routingKey}")
-    public HttpEntity<?> postJsonOnExchange(@PathVariable String exchange, @PathVariable String routingKey, @RequestBody Person message) {
+    public HttpEntity<?> postJsonOnExchange(@PathVariable String exchange,
+                                            @PathVariable String routingKey,
+                                            @RequestBody Person message) throws JsonProcessingException, UnsupportedEncodingException {
         log.info("sending message " + message);
-        rabbitTemplate.convertAndSend(exchange, routingKey, message);
+
+        MessageProperties messageProperties = new MessageProperties();
+        messageProperties.setType(MediaType.APPLICATION_JSON_VALUE);
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonString = objectMapper
+                .writeValueAsString(message);
+        byte[] bytes = jsonString.getBytes("UTF-8");
+        Message messageT = new Message(bytes, messageProperties);
+
+        rabbitTemplate.convertAndSend(exchange, routingKey, messageT);
         return ResponseEntity.ok().build();
     }
 
